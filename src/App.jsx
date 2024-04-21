491ec7b82b331dd1dcb5afa2ef5ac7b1d981e203
Rajat Agrawal <rajat.ace22@sot.pdpu.ac.in>
2024-04-21 11:28:29 +0530

    fixed the routes issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,6 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import Header from "./components/Header";
-// import Hero from "./components/Hero";
-// import Collaboration from "./components/Collaboration";
-// import Roadmap from "./components/Roadmap";
-// import SJF from "./components/SJF";
-// import FIFOPageReplacement from "./components/FIFO";
-// import CLookDiskScheduling from './components/CLook';
-// import DP from './components/DP';
-// import ButtonGradient from "./assets/svg/ButtonGradient";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-//         <Header />
-//         <Hero />
-//         <Collaboration />
-//         <Switch>
-//           <Route exact path="/" component={Roadmap} />
-//           <Route exact path="/SJF" component={SJF} />
-//           <Route exact path="/DP" component={DP} />
-//           <Route exact path="/CLook" component={CLookDiskScheduling} />
-//           <Route exact path="/FIFO" component={FIFOPageReplacement} />
-//         </Switch>
-//       </div>
-//       <ButtonGradient /> 
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-
-
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Collaboration from "./components/Collaboration";
@@ -42,27 +8,28 @@ import Roadmap from "./components/Roadmap";
 import SJF from "./components/SJF";
 import FIFOPageReplacement from "./components/FIFO";
 import CLookDiskScheduling from './components/CLook';
+import DP from './components/DP';
 import ButtonGradient from "./assets/svg/ButtonGradient";
-import DP from './components/DP'
-import ReactDOM from 'react-dom';
 
 const App = () => {
-
   return (
-    <>
+    <Router>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
         <Collaboration />
-        <Roadmap />
-        <CLookDiskScheduling/>
-        <SJF/>
-        <FIFOPageReplacement/> 
-        <DP/>
-        
+        <Roadmap/>
+        {/* Wrap Routes inside Switch */}
+        <Switch>
+          <Route exact path="/" component={Roadmap} />
+          <Route path="/SJF" component={SJF} />
+          <Route path="/DP" component={DP} />
+          <Route path="/CLook" component={CLookDiskScheduling} />
+          <Route path="/FIFO" component={FIFOPageReplacement} />
+        </Switch> 
       </div>
       <ButtonGradient /> 
-    </>
+    </Router>
   );
 };
 
